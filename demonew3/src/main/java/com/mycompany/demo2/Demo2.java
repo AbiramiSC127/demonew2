27d8a4a9fa86b0d3dd533cc8ca4f163b81f79f2c
Abirami <abirami.502202@sxcce.edu.in>
2025-05-04 20:18:14 +0530

    Add files via upload

---
+++ demonew3/src/main/java/com/mycompany/demo2/Demo2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demo2;
+
+/**
+ *
+ * @author HP
+ */
+public class Demo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ demonew3/src/main/java/demo2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+class demo2 {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
